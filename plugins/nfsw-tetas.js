6cfaa2ec19f82ef583e8ca2d7f84f16563f903f6
TOKIO5025 <albertodovi100@gmail.com>
2025-08-08 21:58:49 -0600

    Update nfsw-tetas.js

--- plugins/nfsw-tetas.js
+++ plugins/nfsw-tetas.js
@@ -1,22 +1,31 @@
 /* Comando creado por 🐉𝙉𝙚𝙤𝙏𝙤𝙆𝙮𝙤 𝘽𝙚𝙖𝙩𝙨🐲 & Light Yagami */
 
+//código creado por NEOTOKIO para hinata bot no quites los créditos 
+//github.com/TOKIO5025
+
 import fetch from 'node-fetch';
 
-let handler = async (m, { conn }) => {
-  m.react('🕑');
+const handler = async (m, { conn, command }) => {
+  try {
+    const res = await fetch('https://api.dorratz.com/nsfw/tetas');
+    if (!res.ok) throw new Error(`Error en la API: ${res.status}`);
 
-  let txt = 'Pack🔥🔥🔥';
-  let img = 'https://api.dorratz.com/nsfw/tetas';
+    const buffer = await res.buffer();
 
-  m.react('✅');
-  conn.sendMessage(m.chat, { 
-    image: { url: img }, 
-    caption: txt 
-  }, { quoted: m });
+    await conn.sendMessage(m.chat, {
+      image: buffer,
+      caption: `🔥 Aquí tienes tu ${command} bien rico/a 😏`,
+    }, { quoted: m });
+
+  } catch (err) {
+    console.error(err);
+    m.reply('❌ No pude traer la imagen, la API no respondió bien.');
+  }
 };
 
-handler.help = ['tet'];
+handler.command = ['tetas', 'teta', 'tetona'];
 handler.tags = ['nsfw'];
-handler.command = ['tet', 'pecho'];
+handler.help = ['tetas', 'teta', 'tetona'];
+handler.register = true;
 
 export default handler;
