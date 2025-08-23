abaf5227f0540a8895de4d1f4073ff1bc4b43f48
TOKIO5025 <albertodovi100@gmail.com>
2025-08-23 16:36:52 -0600

    Update sticker-c.js

--- plugins/sticker-c.js
+++ plugins/sticker-c.js
@@ -13,36 +13,39 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
         let q = m.quoted ? m.quoted : m
         let mime = (q.msg || q).mimetype || q.mediaType || ''
 
+        // Si es imagen, video o sticker
         if (/webp|image|video/g.test(mime)) {
             if (/video/g.test(mime) && (q.msg || q).seconds > 18) 
                 return m.reply('⚠️ Uy vos, el video está bien largo 😏 hacelo más cortito, máximo 12 segs, chapina traviesa 😘')
 
             let img = await q.download?.()
-            if (!img) return m.reply(`*A ver, mi chapinita 😘* responde a una imagen para hacer tu sticker, ¿sí? Usa: ${usedPrefix + command}`) 
+            if (!img) return m.reply(`*A ver, mi chapinita 😘* responde a una imagen o sticker para hacer tu sticker, ¿sí? Usa: ${usedPrefix + command}`) 
 
             try {
                 stiker = await sticker(img, false, f, g)
             } catch (e) {
                 console.error(e)
                 let out
-                if (/webp/g.test(mime)) out = await webp2png(img)
+                if (/webp/g.test(mime)) out = await webp2png(img) // sticker → png
                 else if (/image/g.test(mime)) out = await uploadImage(img)
                 else if (/video/g.test(mime)) out = await uploadFile(img)
                 stiker = await sticker(false, out, f, g)
             }
 
         } else if (args[0]) {
+            // Si es URL
             if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
             else return m.reply('Esa URL está malita 😏 pasame una buena, chapina traviesa 😘')
+        } else {
+            return m.reply(`*Ey vos, no me mandaste nada 😏* Responde a una imagen o sticker para hacer tu sticker, chapina traviesa. Usa: ${usedPrefix + command}`)
         }
     } catch (e) {
         console.error(e)
         if (!stiker) stiker = e
     } finally {
         if (stiker) {
+            // Enviar como sticker correctamente
             conn.sendMessage(m.chat, { sticker: stiker }, { quoted: m })
-        } else {
-            return m.reply(`*Ey vos, no me mandaste nada 😏* Responde a una imagen para hacer tu sticker, chapina traviesa. Usa: ${usedPrefix + command}`) 
         }
     }
 }
@@ -55,4 +58,4 @@ export default handler
 
 const isUrl = (text) => {
     return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))
-  }
+}
