520db97604ee7c1a5fb6bf48541666d11996cfd4
TOKIO5025 <albertodovi100@gmail.com>
2025-10-03 16:53:56 -0600

    Update main-sad.js

--- plugins/main-sad.js
+++ plugins/main-sad.js
@@ -3,7 +3,9 @@ const audios = [
   'https://raw.githubusercontent.com/TOKIO5025/Audios/main/sad1.mp3',
   'https://raw.githubusercontent.com/TOKIO5025/Audios/main/sad2.mp3',
   'https://raw.githubusercontent.com/TOKIO5025/Audios/main/sad3.mp3',
-  'https://raw.githubusercontent.com/TOKIO5025/Audios/main/sad4.mp3'
+  'https://raw.githubusercontent.com/TOKIO5025/Audios/main/sad4.mp3',
+  'https://raw.githubusercontent.com/TOKIO5025/Audios/main/sad5.mp3',
+  'https://raw.githubusercontent.com/TOKIO5025/Audios/main/sad6.mp3'
 ];
 
 const handler = async (m, { conn, text }) => {
