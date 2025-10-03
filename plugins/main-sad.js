d7234a6f590e1b6895b0df458314065f4c78b9b3
TOKIO5025 <albertodovi100@gmail.com>
2025-10-03 16:17:04 -0600

    Create main-sad.js

---
+++ plugins/main-sad.js
@@ -0,0 +1,23 @@
+const audios = [
+  'https://raw.githubusercontent.com/TOKIO5025/Audios/main/sad.mp3',
+  'https://raw.githubusercontent.com/TOKIO5025/Audios/main/sad1.mp3',
+  'https://raw.githubusercontent.com/TOKIO5025/Audios/main/sad2.mp3',
+  'https://raw.githubusercontent.com/TOKIO5025/Audios/main/sad3.mp3',
+  'https://raw.githubusercontent.com/TOKIO5025/Audios/main/sad4.mp3'
+];
+
+const handler = async (m, { conn, text }) => {
+  const randomAudio = audios[Math.floor(Math.random() * audios.length)];
+
+  await conn.sendMessage(m.chat, {
+    audio: { url: randomAudio },
+    mimetype: 'audio/mpeg',
+    ptt: true,
+  }, { quoted: m });
+};
+
+// Comandos sin prefijo
+handler.customPrefix = /^(sad|triste)$/i;
+handler.command = new RegExp;
+
+export default handler;
